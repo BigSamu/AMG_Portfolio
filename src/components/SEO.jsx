0970d11e19f5b493a300f9c8931f6c62a7c3f873
BigSamu <valdesgutierrez@gmail.com>
2023-04-10 12:16:46 -0400

    updates metatags

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -5,28 +5,27 @@ import avatar_1 from '/assets/avatar_1.jpeg';
 import metaTags from '../data/metaTags';
 
 const SEO = ({ title }) => {
- 
   return (
     <>
       <Helmet>
         {/* Standard metadata tags */}
         <title>{title}</title>
+        <meta name="title" content={metaTags.title} />
         <meta name="description" content={metaTags.description} />
-        {/* End standard metadata tags */}
-        {/* Facebook tags */}
+
+        {/* Open Graph / Facebook tags */}
         <meta property="og:type" content={metaTags.type} />
         <meta property="og:title" content={metaTags.title} />
         <meta property="og:description" content={metaTags.description} />
         <meta property="og:image" content={metaTags.image} />
         <meta property="og:url" content={metaTags.url} />
         <meta property="og:site_name" content="AMG ART" />
-        {/* End Facebook tags */}
+
         {/* Twitter tags */}
         <meta name="twitter:creator" content={metaTags.name} />
         <meta name="twitter:card" content={metaTags.type} />
         <meta name="twitter:title" content={metaTags.title} />
         <meta name="twitter:description" content={metaTags.description} />
-        {/* End Twitter tags */}
       </Helmet>
     </>
   );
