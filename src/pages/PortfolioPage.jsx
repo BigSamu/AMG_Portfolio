1531c02e238f616aeb67d1f3a858fca8a8d78a97
BigSamu <valdesgutierrez@gmail.com>
2023-04-10 10:45:26 -0400

    adding under construction label

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 import userDetails from '../data/userDetails';
+import under_construction from '/assets/under_construction.png';
 
 const PortfolioPage = () => {
   return (
     <div className="grid place-items-center h-full p-5">
-      <div>Portfolio Page</div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="mb-5">Portfolio Page</h1>
+        <img src={under_construction} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
