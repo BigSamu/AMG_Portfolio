581935a315b2b13ca5b101fe8df75395763fea12
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-14 20:35:59 +0000

    updating brief message

--- src/data/userDetails.js
+++ src/data/userDetails.js
@@ -5,6 +5,6 @@ export default {
   email: 'anamgph@gmail.com',
   phone: '(+569) 92504215',
   instagram: 'https://www.instagram.com/anamariagutierrezart',
-  brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  brief: 'Bienvenidos a mi expresión artística. Aquí encontrarán obras que capturan la esencia de la naturaleza y la profundidad del autoconocimiento, tejidas con mi experiencia y pasión.',
   biography: 'Nacida en Santiago de Chile el 10 de mayo de 1953, Ana María Gutiérrez, hija del arquitecto Jorge Manuel Gutiérrez y la restauradora Paulina Philippi, forjó su trayectoria artística tras licenciarse en Diseño por la Universidad Católica. Su enriquecimiento técnico y artístico se vio influenciado por talleres con figuras como Andrés Baldwin, Francisco Fernández, Concepción Balmes y Gustavo Ross y se ha solidificado desde 2014 bajo la tutoría de Arturo Duclos. Ana María infunde en su arte una profunda contemplación e introspección, no solo de la naturaleza, capturando la vivacidad de la flora y fauna, sino también reflejando una introspección mariana. Esta dualidad en su obra resalta colores, formas y texturas, ofreciendo una experiencia de inmersión sensorial y espiritual. Su talento ha trascendido, llevándola a exponer en diversas y prestigiosas plataformas, entre ellas galerías, universidades e incluso el Congreso de la República de Chile.'
 };
